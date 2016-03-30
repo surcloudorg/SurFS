0a38e6159626da78d6076246f2dea88c320b036d
surcloud <surcloud@sursen.net/>
2016-03-30 10:33:20 +0800

    update

--- SurFS -1.0.0/surfs-nas/web/js/surfs-web-submit.js
+++
@@ -1,9 +0,0 @@
-function showSubmit(url) {
-	var board  = document.getElementById("sidebar");
-	var form = document.createElement("form");
-	var obj = board.appendChild(form);
-	obj.checked = true;
-	form.action = url;
-	form.method = "post";
-	form.submit();
-}
