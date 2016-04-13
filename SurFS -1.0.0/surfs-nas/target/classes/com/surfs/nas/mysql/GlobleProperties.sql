18b65360e7540022240a7e28dd854ab62bb020e4
surcloud <surcloud@sursen.net/>
2016-04-13 10:20:57 +0800

    update

--- SurFS -1.0.0/surfs-nas/target/classes/com/surfs/nas/mysql/GlobleProperties.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE `?` (
-  `config_key` VARCHAR(40) NOT NULL DEFAULT '' COMMENT '',
-  `config_value` VARCHAR(4000) NOT NULL DEFAULT '' COMMENT '',
-  PRIMARY KEY (`config_key`)
-);
--- SurFS -1.0.0/surfs-nas/target/classes/com/surfs/nas/mysql/NodeRegistry.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `?` (
-  `serverHost` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '',
-  `properties` VARCHAR(2000) NOT NULL DEFAULT '' COMMENT '',
-  PRIMARY KEY (`serverHost`)
-);
