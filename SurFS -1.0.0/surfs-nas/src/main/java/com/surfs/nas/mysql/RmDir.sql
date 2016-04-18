8fc56cd77b2e5b7c42c51ba453bd1f13acf4d757
surcloud <surcloud@sursen.net>
2016-04-18 11:21:01 +0800

    Update RmDir.sql

--- SurFS -1.0.0/surfs-nas/src/main/java/com/surfs/nas/mysql/RmDir.sql
+++ SurFS -1.0.0/surfs-nas/src/main/java/com/surfs/nas/mysql/RmDir.sql
@@ -1,4 +1,6 @@
-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 CREATE PROCEDURE `RmDir`(IN _dirid INTEGER(11))
     NOT DETERMINISTIC
     CONTAINS SQL
