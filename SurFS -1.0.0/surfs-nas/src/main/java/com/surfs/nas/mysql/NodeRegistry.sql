c372686a2d9c7947f6e23f6584feb38084a639da
surcloud <surcloud@sursen.net>
2016-04-18 11:20:29 +0800

    Update NodeRegistry.sql

--- SurFS -1.0.0/surfs-nas/src/main/java/com/surfs/nas/mysql/NodeRegistry.sql
+++ SurFS -1.0.0/surfs-nas/src/main/java/com/surfs/nas/mysql/NodeRegistry.sql
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 CREATE TABLE `?` (
   `serverHost` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '',
   `properties` VARCHAR(2000) NOT NULL DEFAULT '' COMMENT '',
